05816262dcbbe1c5d46fcdfb22e343cb22dfcdd0
Kim McCaskill <kimmccaskill92@gmail.com>
2020-04-19 13:59:44 -0600

    Create initial page once clicking projects tab

---
+++ src/components/ProjectsHome/ProjectsHome.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './ProjectsHome.scss'
+
+const ProjectsHome = () => {
+  return (
+    <div>
+      <h2 className='projects-home-title'>My Projects</h2>
+      <p className='projects-description'>Welcome to my projects page. This portfolio website is one of my personal projects so thank you for taking the time to explore!</p>
+      <p className='projects-description'>This website was created with React and Sass. If you are interested in some more projects of mine, feel free to start by clicking one of the project names.</p>
+    </div>
+  )
+}
+
+export default ProjectsHome
