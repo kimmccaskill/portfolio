b6e9fbcc117111251b570bfa47547bfa35c102a3
Kim McCaskill <kimmccaskill92@gmail.com>
2020-04-19 10:32:52 -0600

    Fix target blank rel error

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import { IconContext } from "react-icons"
-import { FaGithub, FaLinkedin, FaInstagram, FaFacebook  } from "react-icons/fa"
-import { Link } from "react-router-dom";
-import './Home.css'
+import { FaGithub, FaLinkedin, FaInstagram  } from "react-icons/fa"
+import './Home.scss'
 
 const Home = () => {
   return (
@@ -10,19 +9,19 @@ const Home = () => {
       <div className='headshot'></div>
       <IconContext.Provider value={{ size: "2em", color: "white" }}>
         <div className='links-container'>
-          <a target="_blank" href='https://www.linkedin.com/in/kimmccaskill/'>
+          <a target="_blank" rel="noopener noreferrer" href='https://www.linkedin.com/in/kimmccaskill/'>
             <FaLinkedin className='link-img'/>
           </a>
-          <a target="_blank" href='https://github.com/kimmccaskill'>
+          <a target="_blank" rel="noopener noreferrer" href='https://github.com/kimmccaskill'>
             <FaGithub />
           </a>
-          <a target="_blank" href='https://www.instagram.com/kimmccaskill/'>
+          <a target="_blank" rel="noopener noreferrer" href='https://www.instagram.com/kimmccaskill/'>
             <FaInstagram />
           </a>
         </div>
       </IconContext.Provider>
       <h1 className='name'>Kim McCaskill</h1>
-      <h2 classname='my-title'>Software Developer</h2>
+      <h2 className='my-title'>Software Developer</h2>
       <p>Quick blurb about me</p>
     </section>
   )
