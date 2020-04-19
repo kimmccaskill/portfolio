94a8f138c232c716bbc77b8ebc2aac7ecd9fd027
Kim McCaskill <kimmccaskill92@gmail.com>
2020-04-18 22:14:36 -0600

    Create setup for routing between pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './components/Home/Home'
+import Projects from './components/Projects/Projects'
+import About from './components/About/About'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="nav-bar">
+        <div className='logo'>KM</div> 
+        <ul className="tab-list">
+          <li className='tab-item'>
+            <Link to="/">HOME</Link>
+          </li>
+          <li className='tab-item'>
+            <Link to="/projects">PROJECTS</Link>
+          </li>
+          <li className='tab-item'> 
+            <Link to="/about">ABOUT</Link>
+          </li>
+        </ul>
+      </div>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/projects">
+          <Projects />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
