5e83a74173f0f6eda637ae44087d3b0bf9181969
Kim McCaskill <kimmccaskill92@gmail.com>
2020-04-19 00:58:50 -0600

    Add classes for purposes of styling

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Project from '../Project/Project'
+import './Projects.css'
 import {
   Switch,
   Route,
@@ -11,28 +12,31 @@ const Projects = () => {
   let { path, url } = useRouteMatch()
 
   return (
-    <aside>
-      <ul>
-        <li>
-          <Link to={`${url}/ticket-cloud`}>Ticket Cloud</Link>
-        </li>
-        <li>
-          <Link to={`${url}/ateball`}>Hangry Ateball</Link>
-        </li>
-        <li>
-          <Link to={`${url}/bluciferbnb`}>Blucifer BNB</Link>
-        </li>
-      </ul>
-
-      <Switch>
-        <Route exact path={path}>
-          <h3>Some blurb about this portfolio being a project and the projects to date. </h3>
-        </Route>
-        <Route path={`${path}/:projectTitle`}>
-          <Project />
-        </Route>
-      </Switch>
-    </aside>
+    <section className='projects-content'>
+      <aside className='projects-nav'>
+        <ul>
+          <li className='project-btn'>
+            <Link to={`${url}/ticket-cloud`}>Ticket Cloud</Link>
+          </li>
+          <li className='project-btn'>
+            <Link to={`${url}/ateball`}>Hangry Ateball</Link>
+          </li>
+          <li className='project-btn'>
+            <Link to={`${url}/bluciferbnb`}>Blucifer BNB</Link>
+          </li>
+        </ul>
+      </aside>
+        <section className='projects-section'>
+          <Switch>
+            <Route exact path={path}>
+              <h3>Some blurb about this portfolio being a project and the projects to date. </h3>
+            </Route>
+            <Route path={`${path}/:projectTitle`}>
+              <Project />
+            </Route>
+          </Switch>
+        </section>
+    </section>
   )
 }
 
