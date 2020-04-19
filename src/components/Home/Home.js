ddc9e916999c61b57263ab248893119ad8b1402c
Kim McCaskill <kimmccaskill92@gmail.com>
2020-04-19 00:58:33 -0600

    Add icons to link to other pages

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import { IconContext } from "react-icons"
+import { FaGithub, FaLinkedin, FaInstagram, FaFacebook  } from "react-icons/fa"
+import { Link } from "react-router-dom";
+import './Home.css'
 
 const Home = () => {
   return (
-    <h2>Kim McCaskill</h2>
+    <section className='content-section'>
+      <div className='headshot'></div>
+      <IconContext.Provider value={{ size: "2em", color: "white" }}>
+        <div className='links-container'>
+          <a target="_blank" href='https://www.linkedin.com/in/kimmccaskill/'>
+            <FaLinkedin className='link-img'/>
+          </a>
+          <a target="_blank" href='https://github.com/kimmccaskill'>
+            <FaGithub />
+          </a>
+          <a target="_blank" href='https://www.instagram.com/kimmccaskill/'>
+            <FaInstagram />
+          </a>
+        </div>
+      </IconContext.Provider>
+      <h1 className='name'>Kim McCaskill</h1>
+      <h2 classname='my-title'>Software Developer</h2>
+      <p>Quick blurb about me</p>
+    </section>
   )
 }
 
