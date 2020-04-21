440ded2066a02ee3c82832723fb66c3d9d5f018e
Kim McCaskill <kimmccaskill92@gmail.com>
2020-04-21 15:01:33 -0600

    Add styled components for NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,24 +1,61 @@
 import React from 'react';
-import '../App.scss';
-import {
-  Link
-} from "react-router-dom";
+import { Link } from "react-router-dom";
 import PageTab from './PageTab'
+import styled from 'styled-components'
 
 const NavBar = () => {
+  const Navigation = styled.div`
+    display: flex;
+    justify-content: center;
+  `
+  const NavContent = styled.div`
+    width: 1200px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    @media (max-width: 1250px) {
+      width: 90%;
+    }
+    @media (max-width: 600px) {
+      flex-wrap: wrap;
+      justify-content: center;
+    }
+  `
+  const Logo = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #fff;
+    font-size: 1.5em;
+    background-color: #000065;
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    border: 1px solid black;
+    font-family: 'Gill Sans';
+    font-size: .9em;
+    margin: 0px;
+  `
+  const TabList = styled.ul`
+    display: flex;
+    justify-content: flex-end;
+    @media (max-width: 600px) {
+      justify-content: center;
+  }
+  `
   return (
-      <div className='nav-bar'>
-        <div className='nav-content'>
+      <Navigation>
+        <NavContent>
           <Link exact to="/">
-            <div className='logo'><p className='logo-txt'>KM</p></div> 
+            <Logo>KM</Logo> 
           </Link>
-          <ul className="tab-list">
+          <TabList>
             <PageTab tabName='HOME' path='/'/>
             <PageTab tabName='PROJECTS' path='/projects'/>
             <PageTab tabName='ABOUT' path='/about'/>
-          </ul>
-        </div>
-      </div>
+          </TabList>
+        </NavContent>
+      </Navigation>
   );
 }
 
