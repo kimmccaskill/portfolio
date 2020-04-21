a3e55276b62cfa942c27a65b06f27939ff4e7c86
Kim McCaskill <kimmccaskill92@gmail.com>
2020-04-21 10:29:31 -0600

    Break out navbar to sep component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,20 +1,13 @@
 import React from 'react';
-import './App.scss';
+import '../App.scss';
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
+  NavLink,
+  Link
 } from "react-router-dom";
-import Home from './components/Home/Home'
-import Projects from './components/Projects/Projects'
-import About from './components/About/About'
-import NavBar from './components/NavBar'
 
-const App = () => {
+const NavBar = () => {
   return (
-    <Router>
-      <NavBar />
-      {/* <div className='nav-bar'>
+      <div className='nav-bar'>
         <div className='nav-content'>
           <Link exact to="/">
             <div className='logo'><p className='logo-txt'>KM</p></div> 
@@ -53,24 +46,8 @@ const App = () => {
             </li>
           </ul>
         </div>
-      </div> */}
-      <div style={{display: 'flex', justifyContent: 'center'}}>
-        <main className='content'>
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/projects">
-              <Projects />
-            </Route>
-            <Route path="/about">
-              <About />
-            </Route>
-          </Switch>
-        </main>
       </div>
-    </Router>
   );
 }
 
-export default App;
+export default NavBar
