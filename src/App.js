e5b288b389fabe8884fdc5c7e4640b0274b7bc06
Kim McCaskill <kimmccaskill92@gmail.com>
2020-04-21 11:09:03 -0600

    Break out nav bar and page tab to separate components

--- src/App.js
+++ src/App.js
@@ -14,46 +14,6 @@ const App = () => {
   return (
     <Router>
       <NavBar />
-      {/* <div className='nav-bar'>
-        <div className='nav-content'>
-          <Link exact to="/">
-            <div className='logo'><p className='logo-txt'>KM</p></div> 
-          </Link>
-          <ul className="tab-list">
-            <li className='tab-item'>
-              <NavLink 
-                exact
-                to="/" 
-                activeStyle={{
-                  color: "#fff",
-                  paddingBottom: '10px',
-                  borderBottom: '1px solid white'
-                }}
-              >HOME</NavLink>
-            </li>
-            <li className='tab-item'>
-              <NavLink 
-                to="/projects"
-                activeStyle={{
-                  color: "#fff",
-                  paddingBottom: '10px',
-                  borderBottom: '1px solid white'
-                }}
-              >PROJECTS</NavLink>
-            </li>
-            <li className='tab-item'> 
-              <NavLink 
-                to="/about"
-                activeStyle={{
-                  color: "#fff",
-                  paddingBottom: '10px',
-                  borderBottom: '1px solid white'
-                }}
-              >ABOUT</NavLink>
-            </li>
-          </ul>
-        </div>
-      </div> */}
       <div style={{display: 'flex', justifyContent: 'center'}}>
         <main className='content'>
           <Switch>
